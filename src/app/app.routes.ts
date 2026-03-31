4e4fcf6351bfbc1eefacbc07c73b2fdcffb84f2d
Levin-fischer <levin.fischer@brackalltron.ch>
2026-03-31 16:06:44 +0200

    not found erstellt sowie die route angepasst

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //Das sollte zu unterst stehen, damit die anderen Routen zuerst geprüft werden. Ich sött nöd vergässe das nachher ahpasse
+    path: '**',
+    loadComponent: () => import('./not-found/not-found.component').then((m) => m.NotFoundComponent),
+  },
+];
